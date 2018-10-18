26a9f59abddbe7102cf55ed950efa34e4fea397b
lisa <39152394+lisa2308@users.noreply.github.com>
2018-10-18 20:05:10 +0200

    recyclerview

---
+++ app/src/main/java/com/example/lisap/mynews/AdapterRecycler.java
@@ -0,0 +1,4 @@
+package com.example.lisap.mynews;
+
+public class AdapterRecycler {
+}
--- app/src/main/java/com/example/lisap/mynews/MainActivity.java
+++ app/src/main/java/com/example/lisap/mynews/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.lisap.mynews;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MyAdapter mAdapter;
     private ViewPager mPager;
+    private TabLayout mTabLayout;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new MyAdapter(getSupportFragmentManager());
         mPager = findViewById(R.id.activity_main_viewpager);
         mPager.setAdapter(mAdapter);
+
+        mTabLayout = findViewById(R.id.activity_main_tablayout);
+        mRecyclerView = findViewById(R.id.activity_main_recycler_view);
+
     }
 }
